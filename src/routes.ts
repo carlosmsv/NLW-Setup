25a85f8e68057de1261ada82e6115ea54fe5e5c8
Carlos <carlos.msv@hotmail.com>
2023-01-18 07:15:35 -0300

    async function appRoutes

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from "./lib/prisma"
 
-export function appRoutes(app: FastifyInstance){
+export async function appRoutes(app: FastifyInstance){
   app.get('/', async () => {
     const habits = await prisma.habit.findMany()
     
     return habits
   })
 }
-
