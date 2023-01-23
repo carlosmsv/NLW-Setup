d1b2aea654f1e0c733f0b46bcf6ce01567d7bb5c
Carlos <carlos.msv@hotmail.com>
2023-01-22 21:03:18 -0300

    route to toggle a habit on a day

--- src/routes.ts
+++ src/routes.ts
@@ -80,5 +80,44 @@ export async function appRoutes(app: FastifyInstance){
 
     const { id } = toggleHabitParams.parse(request.params)
 
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where:{
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      }
+    })
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      })
+    }
   })
 }
